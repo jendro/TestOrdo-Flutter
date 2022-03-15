ae407f1e8fe32928239c4310bb5b89ca10b8ed61
Jendro Purnomo <jendropurnomo@gmail.com>
2022-03-16 00:11:08 +0700

    fix(slider): fix image slider

--- lib/ui/widget/image_slider.dart
+++ lib/ui/widget/image_slider.dart
@@ -4,7 +4,13 @@ import 'package:test_ordo/ui/misc/colors_app.dart';
 import 'package:test_ordo/ui/misc/dp.dart';
 
 class ImageSlider extends StatefulWidget {
-  ImageSlider({Key? key}) : super(key: key);
+  int height;
+  List<String> imgList;
+  ImageSlider({
+    Key? key,
+    required this.imgList,
+    this.height = 404,
+  }) : super(key: key);
 
   @override
   State<ImageSlider> createState() => _ImageSliderState();
@@ -13,68 +19,21 @@ class ImageSlider extends StatefulWidget {
 class _ImageSliderState extends State<ImageSlider> {
   late DP dp;
   final CarouselController _controller = CarouselController();
-  final List<String> imgList = [
-    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-  ];
   int _current = 0;
 
   @override
   Widget build(BuildContext context) {
     dp = DP(context);
 
-    final List<Widget> imageSliders = imgList
-        .map((item) => Container(
-              child: Container(
-                margin: EdgeInsets.all(5.0),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                    child: Stack(
-                      children: <Widget>[
-                        Image.network(item, fit: BoxFit.cover, width: 1000.0),
-                        Positioned(
-                          bottom: 0.0,
-                          left: 0.0,
-                          right: 0.0,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: [
-                                  Color.fromARGB(200, 0, 0, 0),
-                                  Color.fromARGB(0, 0, 0, 0)
-                                ],
-                                begin: Alignment.bottomCenter,
-                                end: Alignment.topCenter,
-                              ),
-                            ),
-                            padding: EdgeInsets.symmetric(
-                                vertical: 10.0, horizontal: 20.0),
-                            child: Text(
-                              'No. ${imgList.indexOf(item)} image',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    )),
-              ),
-            ))
-        .toList();
-
     return Container(
       child: Column(
         children: [
           CarouselSlider(
             carouselController: _controller,
-            items: imageSliders,
+            items: _listImage(),
             options: CarouselOptions(
               autoPlay: true,
-              height: dp.size(404),
+              height: dp.size(widget.height),
               viewportFraction: 0.88,
               enlargeCenterPage: true,
               onPageChanged: (index, reason) {
@@ -86,7 +45,7 @@ class _ImageSliderState extends State<ImageSlider> {
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: imgList.asMap().entries.map(
+            children: widget.imgList.asMap().entries.map(
               (entry) {
                 return GestureDetector(
                   onTap: () => _controller.animateToPage(entry.key),
@@ -113,5 +72,27 @@ class _ImageSliderState extends State<ImageSlider> {
         ],
       ),
     );
+  }
+
+  List<Widget> _listImage() {
+    return widget.imgList
+        .map(
+          (item) => Container(
+            child: Container(
+              margin: EdgeInsets.all(5.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(dp.size(20)),
+                ),
+                child: Image.network(
+                  item,
+                  fit: BoxFit.cover,
+                  width: dp.full(),
+                ),
+              ),
+            ),
+          ),
+        )
+        .toList();
   }
 }
